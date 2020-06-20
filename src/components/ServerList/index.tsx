37d65b6f291776e65b69becd1689964f96ae5e15
Yago Cunha <yago.cunha123@gmail.com>
2020-06-19 21:21:44 -0300

    Initial commit and first element server list

---
+++ src/components/ServerList/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container, Separator } from './styles'
+
+import ServerButton from '../ServerButton'
+
+const ServerList: React.FC = () => {
+  return (
+    <Container>
+      <ServerButton isHome/>
+
+      <Separator />
+
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton mentions={3} />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton />
+      <ServerButton hasNotifications />
+      <ServerButton />
+      <ServerButton mentions={30} />
+      <ServerButton />
+
+    </Container>
+  )
+}
+
+export default ServerList
