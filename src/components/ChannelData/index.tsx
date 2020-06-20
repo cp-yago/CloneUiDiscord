c76570497481e064c01cb64676e60f87e33281fd
Yago Cunha <yago.cunha123@gmail.com>
2020-06-20 15:31:59 -0300

    ChannelMessages component

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useRef, useEffect} from 'react'
+
+import ChannelMessage, {Mention} from '../ChannelMessage'
 
 import {Container, Messages, InputWrapper, Input, InputIcon} from './styles'
 
 const ChannelData: React.FC = () => {
+  const messagesRef = useRef() as React.MutableRefObject<HTMLDivElement>;
+
+  useEffect(() => {
+    const div = messagesRef.current;
+    if (div) {
+      div.scrollTop = div.scrollHeight;
+    }
+  }, [messagesRef])
+
   return (
     <Container>
-      <Messages />
+      <Messages ref={messagesRef}>
+        {Array.from(Array(10).keys()).map((n) => (
+          <ChannelMessage
+            author="Yago Cunha"
+            date="19/08/2020"
+            content="Hoje é o meu aniversário"
+          />
+        ))}
+
+        <ChannelMessage
+          author="Diego Fernandes"
+          date="19/08/2020"
+          content={
+            <>
+            <Mention>@Yago Cunha</Mention>, Parabéns!!!
+            </>
+          }
+          hasMention
+          isBot  
+        />
+
+      </Messages>
 
       <InputWrapper>
         <Input placeholder="Conversar em #chat-livre" />
