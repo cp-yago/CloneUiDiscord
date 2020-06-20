b44c7caa037ce16c9209e05f337a2948b921d054
Yago Cunha <yago.cunha123@gmail.com>
2020-06-20 14:18:51 -0300

    ChannelData component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {Grid} from './styles'
+import { Grid } from './styles'
 
 import ServerList from '../ServerList'
 import ServerName from '../ServerName'
@@ -8,6 +8,7 @@ import ChannelInfo from '../ChannelInfo'
 import ChannelList from '../ChannelList'
 import UserInfo from '../UserInfo'
 import UserList from '../UserList'
+import ChannelData from '../ChannelData'
 
 const Layout: React.FC = () => {
   return (
@@ -17,9 +18,10 @@ const Layout: React.FC = () => {
       <ChannelInfo />
       <ChannelList />
       <UserInfo />
-      <div />
+      <ChannelData />
       <UserList />
     </Grid>
   )
 }
+
 export default Layout
